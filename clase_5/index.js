6f632655bb9852d711180c41c16671745c1a8fbb
Mauricio Duque Orozco <mauricio.duque.eje@gmail.com>
2020-06-29 21:08:00 -0500

    button on click funct

--- clase_5/index.js
+++ clase_5/index.js
@@ -1,11 +1,38 @@
 var miStorage = window.localStorage;
+var ctrl = {
+  x: 0,
+  y: 0,
+}
+
+var xRect = 10
+var yRect = 0
+
+const clickUp = () => {
+  if(ctrl.y < 500) {
+    ctrl.y++
+    yRect = ctrl.y++
+  }
+}
+
+const clickDwn = () => {
+  if(ctrl.y >= 0) {
+    ctrl.y--
+    yRect = ctrl.y++
+  }
+  
+}
 
 window.onload = () => {
   var cnvs = document.getElementById('canvasGame')
+  // var upBtn = document.getElementById('up')
+  // var lftBtn = document.getElementById('lft')
+  // var rghBtn = document.getElementById('rgh')
+  // var dwnBtn = document.getElementById('dwn')
+
+  
+
   var ctx = cnvs.getContext('2d')
   // cnvs.width = 800
-  var xRect = 0
-  var yRect = 0
   
   const clearRect = () => {
     ctx.fillStyle = 'white'
@@ -20,8 +47,14 @@ window.onload = () => {
   setInterval(() => {
     miStorage.setItem('@pos_x', xRect);
     miStorage.setItem('@pos_y', yRect);
+
+    let pos = {
+      x: miStorage.getItem('@pos_x'),
+      y: miStorage.getItem('@pos_y'),
+    }
+    // console.log(pos)
     clearRect()
-    dibujarRect(xRect++, yRect++)
+    dibujarRect(xRect, yRect)
   }, 500)
 }
 
